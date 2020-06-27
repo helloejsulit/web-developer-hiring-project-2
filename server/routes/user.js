02d4c1ab7971abb7bd5488b4a949ae37a4120215
EJ Sulit <mailbox@helloejsulit.com>
2020-06-28 06:13:10 +0800

    added user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const userData = require("../data/userData.json");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/:id", (req, res, next) => {
+  const user = userData.data.find(
+    (user) => user.userId === parseInt(req.params.id)
+  );
+
+  res.json(user);
+});
+
+router.post("/:id/policy", (req, res, next) => {
+  const user = userData.data.find(
+    (user) => user.userId === parseInt(req.params.id)
+  );
+
+  res.json(user.policy);
+});
+
+router.post("/:id/schedule", (req, res, next) => {
+  const user = userData.data.find(
+    (user) => user.userId === parseInt(req.params.id)
+  );
+
+  res.json(user.policy.policySchedule);
+});
+
+router.post("/:id/history", (req, res, next) => {
+  const user = userData.data.find(
+    (user) => user.userId === parseInt(req.params.id)
+  );
+
+  res.json(user.policy.policyHistory);
 });
 
 module.exports = router;
